31a6e38076b06c4a6a98d35102abea277c5deadb
Lal Akın <71639446+lalakin99@users.noreply.github.com>
2020-12-26 14:21:29 +0300

    Add files via upload

---
+++ App.R
@@ -0,0 +1,56 @@
+library(shiny)
+library(datasets)
+
+mpgData <- mtcars
+ui <- fluidPage(
+  titlePanel("IE 421 Homework- Lal Akın"),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      selectInput("variable", "Variable:",
+                  c("Cylinders" = "cyl",
+                    "Transmission" = "am",
+                    "Gears" = "gear")),
+      checkboxInput("outliers", "Show outliers", TRUE)
+      
+    ),
+    
+    mainPanel(
+      
+      h3(textOutput("caption")),
+      
+      plotOutput("mpgPlot")
+      
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  formulaText <- reactive({
+    paste("mpg ~", input$variable)
+  })
+  
+  output$caption <- renderText({
+    formulaText()
+  })
+  
+
+  output$mpgPlot <- renderPlot({
+  ggplot(data=mpgData, aes(x=mpg)) + 
+      geom_histogram(binwidth=5,col= "mistyrose", 
+                     fill="thistle4",alpha = 1) + facet_wrap(~mpgData[[input$variable]], nrow = 3)
+  })
+  
+}
+shinyApp(ui, server)
+
+
+
+
+
+
+
+
